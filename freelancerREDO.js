7824a03145aac451e6a164534b1a1bf09194a3a3
bekah <bekahritter@gmail.com>
2024-04-12 09:33:39 -0700

    I THINK I FIXED ITgit add .git add .

--- freelancerREDO.js
+++ freelancerREDO.js
@@ -6,26 +6,28 @@ cashPrice.classList.add("p");
 
 // appending p after the heading makes sure the heading goes first?
 
-const freelancers = [
+const workers = [
   { name: "Dr. Slice", price: 25, occupation: "gardener" },
   { name: "Dr. Pressure", price: 51, occupation: "programmer" },
   { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+];
+
+const randomWorkers = [
   { name: "Prof. Prism", price: 81, occupation: "teacher" },
   { name: "Dr. Impulse", price: 43, occupation: "teacher" },
   { name: "Prof. Spark", price: 76, occupation: "programmer" },
   { name: "Dr. Wire", price: 47, occupation: "teacher" },
   { name: "Prof. Goose", price: 72, occupation: "driver" },
+  { name: "Brian Mui", price: 50, occupation: "mentor" },
 ];
-
-const workers = [];
 
 const freelancerList = document.querySelector("#freelanceWorkers");
 
 let interval = setInterval(addWorkers, 3000);
 
 function addWorkers() {
-  const randomIndex = Math.floor(Math.random() * freelancers.length);
-  workers.push(freelancers[randomIndex]);
+  const randomIndex = Math.floor(Math.random() * randomWorkers.length);
+  workers.push(randomWorkers[randomIndex]);
 
   render();
 }
@@ -50,19 +52,20 @@ function render() {
     let price = getPrice();
 
     function getPrice() {
-      let array = [];
       let totalPrice = 0;
-      let money = workers.map((worker) => worker.price);
-      let moneyString = (totalPrice += money);
-      array.push(moneyString);
-      console.log(array);
+      let length = workers.length;
+      let money = workers.map((worker) => (totalPrice += worker.price));
+      console.log(totalPrice);
+      return totalPrice;
+
+      totalPrice = 0;
     }
 
     //i know this part is not correct but I just could not figure this out.
     //reduce/map/for. just not sure. maybe we can go over sometime.
-    cashPrice.textContent = `The going rate is ${workers.map(
-      (worker) => worker.price
-    )}/${workers.length}`;
+    cashPrice.textContent = `The going rate is $${Math.round(
+      getPrice() / workers.length
+    )}`;
     heading.append(cashPrice);
 
     //stop interval from going forever ever
